1b8abcf70acbee193c6b570b5b39e67557caaae1
SavindaJayasekara <thantrige32@gmail.com>
2023-06-15 13:03:39 +0530

    added launcer

---
+++ src/main/java/Launcher.java
@@ -0,0 +1,8 @@
+import javafx.application.Application;
+import lk.ijse.gmaisender.AppInitializer;
+
+public class Launcher {
+    public static void main(String[] args) {
+        AppInitializer.main(args);
+    }
+}
--- src/main/java/lk/ijse/gmaisender/AppInitializer.java
+++ src/main/java/lk/ijse/gmaisender/AppInitializer.java
@@ -8,4 +8,8 @@ public class AppInitializer extends Application {
     public void start(Stage primaryStage) throws Exception {
 
     }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
