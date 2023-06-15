6864c4ed3b55749c21549e881651cc340506fb57
SavindaJayasekara <thantrige32@gmail.com>
2023-06-15 13:30:21 +0530

    added fxml forms

--- src/main/java/lk/ijse/gmaisender/controller/HomeFormController.java
+++ src/main/java/lk/ijse/gmaisender/controller/HomeFormController.java
@@ -1,13 +1,16 @@
 package lk.ijse.gmaisender.controller;
 
+import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 
 public class HomeFormController {
-    public TextField txtGmail;
-    public ImageView btnNext;
+    @FXML
+    private TextField txtGmail;
+    @FXML
+    private ImageView btnNext;
 
     public void txtGmailKeyTyped(KeyEvent keyEvent) {
 
--- src/main/java/lk/ijse/gmaisender/controller/SenderFormController.java
+++ src/main/java/lk/ijse/gmaisender/controller/SenderFormController.java
@@ -1,4 +1,29 @@
 package lk.ijse.gmaisender.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
 public class SenderFormController {
+    @FXML
+    private Hyperlink hypGmail;
+    @FXML
+    private TextArea ariaMessage;
+    @FXML
+    private TextField txtGmail;
+    @FXML
+    private ImageView btnSend;
+
+    public void mouseExit(MouseEvent event) {
+    }
+
+    public void mouseEnter(MouseEvent event) {
+    }
+
+    public void btnSendOnAction(MouseEvent event) {
+
+    }
 }
