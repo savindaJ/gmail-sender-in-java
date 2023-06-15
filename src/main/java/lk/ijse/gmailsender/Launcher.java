a093697c348d9b9dd350a7424dc911d5f169585b
SavindaJayasekara <thantrige32@gmail.com>
2023-06-15 12:54:36 +0530

    added fxml and controllers

---
+++ src/main/java/lk/ijse/gmailsender/Launcher.java
@@ -0,0 +1,7 @@
+package lk.ijse.gmailsender;
+
+public class Launcher {
+    public static void main(String[] args) {
+        AppInitializer.main(args);
+    }
+}
---
+++ src/main/java/lk/ijse/gmailsender/controller/SendmaleFormController.java
@@ -0,0 +1,4 @@
+package lk.ijse.gmailsender.controller;
+
+public class SendmaleFormController {
+}
