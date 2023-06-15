043c932c17832ea9e722772f7a3dbd289c4b3364
SavindaJayasekara <thantrige32@gmail.com>
2023-06-15 13:18:11 +0530

    added home

--- src/main/java/lk/ijse/gmaisender/controller/HomeFormController.java
+++ src/main/java/lk/ijse/gmaisender/controller/HomeFormController.java
@@ -1,4 +1,25 @@
 package lk.ijse.gmaisender.controller;
 
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+
 public class HomeFormController {
+    public TextField txtGmail;
+    public ImageView btnNext;
+
+    public void txtGmailKeyTyped(KeyEvent keyEvent) {
+
+    }
+
+    public void btnNextOnAction(MouseEvent event) {
+    }
+
+    public void mouseEnter(MouseEvent event) {
+    }
+
+    public void mouseExit(MouseEvent event) {
+
+    }
 }
