afef45142978f7fce799c60b29324eaa55c84513
SavindaJayasekara <thantrige32@gmail.com>
2023-06-15 13:34:44 +0530

    'modifed'

--- src/main/java/lk/ijse/gmaisender/AppInitializer.java
+++ src/main/java/lk/ijse/gmaisender/AppInitializer.java
@@ -1,12 +1,20 @@
 package lk.ijse.gmaisender;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class AppInitializer extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-
+        Parent root = FXMLLoader.load(this.getClass().getResource("/view/home_form.fxml"));
+        Scene mainScene = new Scene(root);
+        primaryStage.setScene(mainScene);
+        primaryStage.setTitle("your mail");
+        primaryStage.centerOnScreen();
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
